485bc9af2424808dc1dd25102d1774b70df3c7d3
Luke Titley <luke@titley.com>
2021-05-24 22:19:40 +0200

    Added dispatch mechanism

--- asttoc/src/cppmm_stl_remap.cpp
+++ asttoc/src/cppmm_stl_remap.cpp
@@ -6,17 +6,46 @@
 namespace cppmm {
 namespace remap {
 
+namespace libcpp {
+
+//------------------------------------------------------------------------------
+// libcpp/clang
+//------------------------------------------------------------------------------
+Remapped namespace_(const std::string& qualified, const std::string& short_) {
+
+    return Remapped{qualified, short_};
+}
+
+Remapped record(const std::string& qualified, const std::string& short_) {
+    return Remapped{qualified, short_};
+}
+} // namespace libcpp
+
+//------------------------------------------------------------------------------
+// Entry point
+//------------------------------------------------------------------------------
 // Namespace
 Remapped namespace_(StandardTemplateLib stl, const std::string& qualified,
                     const std::string& short_) {
-
-    return Remapped{qualified, short_};
+    switch (stl) {
+    case StandardTemplateLib::kstdcpp:
+        return libcpp::namespace_(qualified, short_);
+    // case StandardTemplateLib::kcpp:
+    default:
+        return Remapped{qualified, short_};
+    }
 }
 
 // Record
 Remapped record(StandardTemplateLib stl, const std::string& qualified,
                 const std::string& short_) {
-    return Remapped{qualified, short_};
+    switch (stl) {
+    case StandardTemplateLib::kstdcpp:
+        return libcpp::record(qualified, short_);
+    // case StandardTemplateLib::kcpp:
+    default:
+        return Remapped{qualified, short_};
+    }
 }
 
 } // namespace remap
